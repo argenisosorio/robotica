bb5dd3c0a297e04ad788dc9aa670bb1842df4b70
Argenis Osorio <argenisosorio580@hotmail.com>
2025-07-30 15:42:04 -0400

    feat(styles/static): Correcciones en estylos de media queries y actualizados algunos recursos estáticos

--- aprendizaje_robotica/ruta_2/static/js/loader.js
+++ aprendizaje_robotica/ruta_2/static/js/loader.js
@@ -1,9 +1,14 @@
+/**
+ * Evento que se ejecuta cuando la ventana ha terminado de cargar.
+ * Detiene la animación del spinner y realiza una transición suave para ocultarlo.
+ * Finalmente, oculta completamente el contenedor del spinner después de la transición.
+ */
 window.addEventListener('load', function() {
   var spinnerContainer = document.getElementById('spinnerContainer');
   var spinnerImg = document.querySelector('.spinner-img');
 
-    spinnerImg.classList.add('stop-spin'); // Detiene la animación
-    spinnerContainer.style.opacity = '0';   // Inicia el desvanecimiento
+  spinnerImg.classList.add('stop-spin'); // Detiene la animación
+  spinnerContainer.style.opacity = '0';   // Inicia el desvanecimiento
 
   // Opcional: después de la transición, oculta el spinner completamente
   setTimeout(function() {
